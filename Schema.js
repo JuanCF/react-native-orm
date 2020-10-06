1edcdc83c6df2481bee03144d35708bd8b79eaf2
Juan Carlos Flores <jcflores_martineznajam@hotmail.com>
2020-10-05 22:27:40 -0600

    Implementing automigrate and fixing issue with alterTable

--- Schema.js
+++ Schema.js
@@ -2,11 +2,11 @@ import SQLite from 'react-native-sqlite-storage';
 
 import { toSqlField } from './utils/fields';
 
-let _databaseName       = new WeakMap();
-let _version            = new WeakMap();
-let _description        = new WeakMap();
-let _size               = new WeakMap();
-let _databaseInstance   = new WeakMap();
+let _databaseName = new WeakMap();
+let _version = new WeakMap();
+let _description = new WeakMap();
+let _size = new WeakMap();
+let _databaseInstance = new WeakMap();
 
 export class Schema {
     constructor(props = {}) {
@@ -21,7 +21,7 @@ export class Schema {
         // Database configuration
         _databaseName.set(this, props.databaseName);
         _version.set(this, props.version || '1.0');
-        _description.set(this, props.description || `${ _databaseName.get(this) }; Version: ${ _version.get(this) }`);
+        _description.set(this, props.description || `${_databaseName.get(this)}; Version: ${_version.get(this)}`);
         _size.set(this, props.size || -1);
         _databaseInstance.set(this, null);
 
@@ -58,6 +58,60 @@ export class Schema {
         }
     }
 
+    automigrate(model) {
+        const automigrateFn = async () => {
+            const fields = {
+                ...model.getModelFields(),
+                created_at: 'string',
+                updated_at: 'string',
+                deleted_at: 'string'
+            };
+
+            const modelFiels = Object.keys(fields);
+
+            function arrayEquals(a, b) {
+                return Array.isArray(a) &&
+                    Array.isArray(b) &&
+                    a.length === b.length &&
+                    a.every((val, index) => val === b[index]);
+            }
+
+            try {
+                const tableInfo = await await (_databaseInstance.get(this)).executeSql(`SELECT name FROM pragma_table_info('${model.getModelName()}');`);
+                const infoColumns = tableInfo[0];
+
+                const currentDefinition = [];
+                for (var x = 0; x < infoColumns.rows.length; x++) {
+                    currentDefinition.push(infoColumns.rows.item(x).name);
+                }
+                if(currentDefinition.length){
+                    if(arrayEquals(modelFiels,currentDefinition)){
+                        return {
+                            statusCode: 200,
+                            message: 'Table successfully created',
+                            data: {
+                                modelName: model.getModelName(),
+                                fields
+                            }
+                        };
+                    }else{
+                        const alterResult = await this.alterTable(model);
+                        return alterResult;
+                    } 
+                }else{
+                    const createResult = await this.createTable(model);
+                    return createResult;
+                }
+            } catch (error) {
+                throw {
+                    statusCode: 500,
+                    message: error.message
+                };
+            }
+        };
+        return automigrateFn();
+    }
+
     /**
      * Creates new table via model
      * 
@@ -77,8 +131,8 @@ export class Schema {
 
             // Format to SQL field
             Object.keys(fields).forEach((fieldVal, fieldIndex) => {
-                sqlFieldFormat += `${ fieldVal } ${toSqlField(fields[fieldVal])}`
-                    + ( 
+                sqlFieldFormat += `${fieldVal} ${toSqlField(fields[fieldVal])}`
+                    + (
                         fieldIndex === Object.keys(fields).length - 1
                             ? ''
                             : ', '
@@ -138,12 +192,14 @@ export class Schema {
                 deleted_at: 'string'
             };
 
+            const modelFiels = Object.keys(fields);
+
             let sqlFieldFormat = '';
 
             // Format to SQL field
             Object.keys(fields).forEach((fieldVal, fieldIndex) => {
-                sqlFieldFormat += `${ fieldVal } ${toSqlField(fields[fieldVal])}`
-                    + ( 
+                sqlFieldFormat += `${fieldVal} ${toSqlField(fields[fieldVal])}`
+                    + (
                         fieldIndex === Object.keys(fields).length - 1
                             ? ''
                             : ', '
@@ -154,7 +210,7 @@ export class Schema {
 
                 await (_databaseInstance.get(this)).executeSql('PRAGMA foreign_keys=off;');
 
-                const tableInfo =  await await (_databaseInstance.get(this)).executeSql(`SELECT name FROM pragma_table_info('${model.getModelName()}');`);
+                const tableInfo = await await (_databaseInstance.get(this)).executeSql(`SELECT name FROM pragma_table_info('${model.getModelName()}');`);
 
                 const infoColumns = tableInfo[0];
 
@@ -163,18 +219,19 @@ export class Schema {
                     oldFields.push(infoColumns.rows.item(x).name);
                 }
 
-                const doAlteration = await new Promise((resolve, reject)=>{
-                    (_databaseInstance.get(this)).transaction((tx)=>{
-                        const newTable = 'temp'+ model.getModelName();
+                const doAlteration = await new Promise((resolve, reject) => {
+                    (_databaseInstance.get(this)).transaction((tx) => {
+                        const newTable = 'temp' + model.getModelName();
                         const table = model.getModelName();
-                        const columnList = oldFields.join();
+                        const intersection = modelFiels.filter(x => oldFields.includes(x));
+                        const columnList = intersection.join();
                         tx.executeSql(`CREATE TABLE IF NOT EXISTS ${newTable}(${sqlFieldFormat});`);
                         tx.executeSql(`INSERT INTO ${newTable}(${columnList}) SELECT ${columnList} FROM ${table};`);
                         tx.executeSql(`DROP TABLE ${table};`);
                         tx.executeSql(`ALTER TABLE ${newTable} RENAME TO ${table};`);
-                    },(error)=>{
+                    }, (error) => {
                         reject(error);
-                    },(result)=>{
+                    }, (result) => {
                         resolve(result);
                     });
                 });
